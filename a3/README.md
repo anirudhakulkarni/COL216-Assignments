85ad469f755dd973aa015e5400523ca963a63713
ANIRUDHA KULKARNI <kulkarnianirudha8@gmail.com>
2021-03-12 19:05:55 +0530

    register file implementation

---
+++ a3/README.md
@@ -0,0 +1,7 @@
+TO DO:
+
+1. Raise exception when used anyone from this
+
+![](image/README/1615552249219.png)
+
+2.
--- a3/main.cpp
+++ a3/main.cpp
@@ -1,10 +1,72 @@
 #include "stdio.h"
 #include "iostream"
 #include "fstream"
-using namespace std;
+#include <string>
+#include <sstream>
+#include <bits/stdc++.h>
 
+using namespace std;
+template <class T>
+string to_string(T t, ios_base &(*f)(ios_base &))
+{
+    // Helper function for printRegisterFile
+    ostringstream oss;
+    oss << f << t;
+    return oss.str();
+}
+
+void printRegisterFile(map<string, pair<int, int>> registerFile)
+{
+    // Input: array containing register file
+    // output: prints containts of register file
+    // Assumptions: size of array is 32
+    map<string, pair<int, int>>::iterator itr;
+    cout << "\nThe map registerFile is : \n";
+    cout << "\tKEY\tELEMENT\n";
+    for (itr = registerFile.begin(); itr != registerFile.end(); ++itr)
+    {
+        cout << '\t' << itr->first
+             << '\t' << itr->second.first << '\n';
+    }
+}
+void initializeRegisterFile(map<string, pair<int, int>> &registerFile)
+{
+    registerFile.insert(make_pair("$zero", make_pair(0, 0)));
+    registerFile.insert(make_pair("$at", make_pair(1, 0)));
+    registerFile.insert(make_pair("$v0", make_pair(2, 0)));
+    registerFile.insert(make_pair("$v1", make_pair(3, 0)));
+    registerFile.insert(make_pair("$a0", make_pair(4, 0)));
+    registerFile.insert(make_pair("$a1", make_pair(5, 0)));
+    registerFile.insert(make_pair("$a2", make_pair(6, 0)));
+    registerFile.insert(make_pair("$a3", make_pair(7, 0)));
+    registerFile.insert(make_pair("$t0", make_pair(8, 0)));
+    registerFile.insert(make_pair("$t1", make_pair(9, 0)));
+    registerFile.insert(make_pair("$t2", make_pair(10, 0)));
+    registerFile.insert(make_pair("$t3", make_pair(11, 0)));
+    registerFile.insert(make_pair("$t4", make_pair(12, 0)));
+    registerFile.insert(make_pair("$t5", make_pair(13, 0)));
+    registerFile.insert(make_pair("$t6", make_pair(14, 0)));
+    registerFile.insert(make_pair("$t7", make_pair(15, 0)));
+    registerFile.insert(make_pair("$s0", make_pair(16, 0)));
+    registerFile.insert(make_pair("$s1", make_pair(17, 0)));
+    registerFile.insert(make_pair("$s2", make_pair(18, 0)));
+    registerFile.insert(make_pair("$s3", make_pair(19, 0)));
+    registerFile.insert(make_pair("$s4", make_pair(20, 0)));
+    registerFile.insert(make_pair("$s5", make_pair(21, 0)));
+    registerFile.insert(make_pair("$s6", make_pair(22, 0)));
+    registerFile.insert(make_pair("$s7", make_pair(23, 0)));
+    registerFile.insert(make_pair("$t8", make_pair(24, 0)));
+    registerFile.insert(make_pair("$t9", make_pair(25, 0)));
+    registerFile.insert(make_pair("$k0", make_pair(26, 0)));
+    registerFile.insert(make_pair("$k1", make_pair(27, 0)));
+    registerFile.insert(make_pair("$gp", make_pair(28, 0)));
+    registerFile.insert(make_pair("$sp", make_pair(29, 0)));
+    registerFile.insert(make_pair("$fp", make_pair(30, 0)));
+    registerFile.insert(make_pair("$ra", make_pair(31, 0)));
+}
 int main(int argc, char const *argv[])
 {
+    // Input file section
     ofstream outfile;
     ifstream infile;
     if (argc < 2)
@@ -15,78 +77,10 @@ int main(int argc, char const *argv[])
     {
         infile.open(argv[1]);
     }
-    outfile.open("../output.txt");
-    outfile << "F";
-    cout << argv[1];
+
+    // Declarations
+    map<string, pair<int, int>> registerFile; // Register name => (registr number, Value)
+    initializeRegisterFile(registerFile);
+    printRegisterFile(registerFile);
     return 0;
-}
-int getRegisterNo(string reg)
-{
-    // cout << reg << endl;
-    int regno = -1;
-    if (reg.compare("$zero") == 0)
-        regno = 0;
-    else if (reg.compare("$at") == 0)
-        regno = 1;
-    else if (reg.compare("$v0") == 0)
-        regno = 2;
-    else if (reg.compare("$v1") == 0)
-        regno = 3;
-    else if (reg.compare("$a0") == 0)
-        regno = 4;
-    else if (reg.compare("$a1") == 0)
-        regno = 5;
-    else if (reg.compare("$a2") == 0)
-        regno = 6;
-    else if (reg.compare("$a3") == 0)
-        regno = 7;
-    else if (reg.compare("$t0") == 0)
-        regno = 8;
-    else if (reg.compare("$t1") == 0)
-        regno = 9;
-    else if (reg.compare("$t2") == 0)
-        regno = 10;
-    else if (reg.compare("$t3") == 0)
-        regno = 11;
-    else if (reg.compare("$t4") == 0)
-        regno = 12;
-    else if (reg.compare("$t5") == 0)
-        regno = 13;
-    else if (reg.compare("$t6") == 0)
-        regno = 14;
-    else if (reg.compare("$t7") == 0)
-        regno = 15;
-    else if (reg.compare("$s0") == 0)
-        regno = 16;
-    else if (reg.compare("$s1") == 0)
-        regno = 17;
-    else if (reg.compare("$s2") == 0)
-        regno = 18;
-    else if (reg.compare("$s3") == 0)
-        regno = 19;
-    else if (reg.compare("$s4") == 0)
-        regno = 20;
-    else if (reg.compare("$s5") == 0)
-        regno = 21;
-    else if (reg.compare("$s6") == 0)
-        regno = 22;
-    else if (reg.compare("$s7") == 0)
-        regno = 23;
-    else if (reg.compare("$t8") == 0)
-        regno = 24;
-    else if (reg.compare("$t9") == 0)
-        regno = 25;
-    else if (reg.compare("$k0") == 0)
-        regno = 26;
-    else if (reg.compare("$k1") == 0)
-        regno = 27;
-    else if (reg.compare("$gp") == 0)
-        regno = 28;
-    else if (reg.compare("$sp") == 0)
-        regno = 29;
-    else if (reg.compare("$fp") == 0)
-        regno = 30;
-    else if (reg.compare("$ra") == 0)
-        regno = 31;
-    return regno;
 }
